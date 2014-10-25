77d9a317393b791f43be6ee9f602a27e90dcfa2b
eddiekimosabe <eddiekimosabe@gmail.com>
2014-10-25 19:55:52 -0400

    Setup environment for Oauth
    -Start creating algorithm for estimated time to finish reading

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,37 @@ class User < ActiveRecord::Base
 
   def request_token
     conn = Faraday.new('https://getpocket.com')
-    post = conn.post '/v3/oauth/request', { 'consumer_key' => POCKET_CONSUMER_KEY, 'redirect_uri' => 'localhost:3000'}, { 'X-Accept' => 'application/json' }
+    post = conn.post '/v3/oauth/request', { 'consumer_key' => "POCKET_CONSUMER_KEY", 'redirect_uri' => 'localhost:3000'}, { 'X-Accept' => 'application/json' }
     @code = JSON.parse(post.body)['code']
     return @code
   end
 
   def oauth
     conn = Faraday.new('https://getpocket.com')
-    post = conn.post 'v3/oauth/authorize', { 'consumer_key' => POCKET_CONSUMER_KEY, 'code' => @code }, { 'X-Accept' => 'application/json' }
+    post = conn.post 'v3/oauth/authorize', { 'consumer_key' => "POCKET_CONSUMER_KEY", 'code' => @code }, { 'X-Accept' => 'application/json' }
     puts post
   end
 
+  def estimated_time(word_count)
+  	@time = word_count/200.0 
+  	
+  	if @time >= 60 
+
+  		@hour = (@time/60).floor
+  		@minutes = (@time%60).ceil
+
+  		if @hour == 1 
+
+  			return @hour.to_s + " " + "hour " + @minutes.to_s + " " + "minutes"
+  		else
+  			return @hour.to_s + " " + "hours " + @minutes.to_s + " " + "minutes"
+  		end
+  	else
+  		return @time.to_s + " " + "minutes"
+
+  	end
+
+  end
+
 end
+  	# return time.to_s + " " + "minutes"
