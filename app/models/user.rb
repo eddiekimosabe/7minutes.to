bea600a704765b47612e593179ed158600c4dd86
eddiekimosabe <eddiekimosabe@gmail.com>
2014-10-26 00:41:10 -0400

    Setup environment to hide keys
    -Install figaro gem
    -Remove dotenv gem
    -Remove unnecessary yaml files

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
 
   def request_token
     conn = Faraday.new('https://getpocket.com')
-    post = conn.post '/v3/oauth/request', { 'consumer_key' => POCKET_CONSUMER_KEY, 'redirect_uri' => 'localhost:3000'}, { 'X-Accept' => 'application/json' }
+    post = conn.post '/v3/oauth/request', { 'consumer_key' => ENV['POCKET_CONSUMER_KEY'], 'redirect_uri' => 'localhost:3000'}, { 'X-Accept' => 'application/json' }
+    
     @code = JSON.parse(post.body)['code']
     self.code = @code
     self.save
@@ -17,11 +18,11 @@ class User < ActiveRecord::Base
 
   def oauthd
     conn = Faraday.new('https://getpocket.com')
-    post = conn.post 'v3/oauth/authorize', { 'consumer_key' => POCKET_CONSUMER_KEY, 'code' => self.code }, { 'X-Accept' => 'application/json' }
+    post = conn.post 'v3/oauth/authorize', { 'consumer_key' => ENV['POCKET_CONSUMER_KEY'], 'code' => self.code }, { 'X-Accept' => 'application/json' }
     self.access_token = JSON.parse(post.body)['access_token']
     self.save
     conn = Faraday.new('https://getpocket.com')
-    post = conn.post 'v3/get', { 'consumer_key' => "33723-f10bda2c27d5146c6a5b658f", 'access_token' => self.access_token, "count" => "10", "detailType" => "complete" }, { 'X-Accept' => 'application/json' }
+    post = conn.post 'v3/get', { 'consumer_key' => ENV['POCKET_CONSUMER_KEY'], 'access_token' => self.access_token, "count" => "10", "detailType" => "complete"}, { 'X-Accept' => 'application/json' }
     puts "*"*50
     puts post.inspect
     puts "*"*50
