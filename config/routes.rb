75494c0aad35e5c0b526c294b8c940a047e08c1c
David Bragdon <bragdon@me.com>
2014-10-28 13:18:47 -0400

    Change /success to oauth/success and point it to #success instead of #show

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ devise_for :users, :controllers => { :registrations => "registrations" }  # The 
   get '/users/:id/profile' => 'profile#edit', as: :profile
   post 'users/:id/profile' => 'profile#update'
   get '/oauth' => 'profile#oauth'
-  get '/success' => 'profile#show'
+  get '/oauth/success' => 'profile#success'
 
 
   # Example of regular route:
